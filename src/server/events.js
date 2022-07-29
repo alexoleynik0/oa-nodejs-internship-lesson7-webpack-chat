b60aca9d5fe48494c70a6094b6d4f8f399cd69a8
Alex Oleynik <oleynik.webdev@gmail.com>
2022-07-30 01:10:15 +0300

    feat(server)!: added server express app runner;

---
+++ src/server/events.js
@@ -0,0 +1,50 @@
+const logger = require('../helpers/logger');
+
+/**
+ * @function
+ * @param  {NodeJS.ErrnoException} error
+ * @returns throw error
+ */
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  switch (error.code) {
+    case 'EACCES':
+      logger('Port requires elevated privileges', 'server.onError event', 'error');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      logger('Port is already in use', 'server.onError event', 'error');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+/**
+ * @function
+ * @inner
+ * @description log port to console
+ */
+function onListening() {
+  const addr = this.address();
+  const bindStr = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
+
+  logger(`Listening on ${bindStr}`, 'server.onListening event', 'log');
+}
+
+/**
+ * @function
+ * @inner
+ * @param {http.Server} server
+ */
+function bindServer(server) {
+  server.on('error', (error) => onError.bind(server)(error));
+  server.on('listening', onListening.bind(server));
+}
+
+module.exports = {
+  bindServer,
+};
---
+++ src/server/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+require('./dotenv');
+const events = require('./events');
+const server = require('./server');
+
+const port = server.get('port');
+
+events.bindServer(
+  http.createServer(server).listen(port),
+);

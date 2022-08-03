ba312c6069aa345a025644dec7bc71966bb8ebae
Alex Oleynik <oleynik.webdev@gmail.com>
2022-08-03 21:41:07 +0300

    chore(helper): restResponse helper added;

---
+++ src/helpers/http/restResponse.js
@@ -0,0 +1,19 @@
+const AuthError = require('../../error/AuthError');
+const ResourceNotFoundError = require('../../error/ResourceNotFoundError');
+
+function checkResourceIsFound(resource, errorMessage) {
+  if (resource === null) {
+    throw new ResourceNotFoundError(errorMessage);
+  }
+}
+
+function checkUserIsAuth(user, errorMessage) {
+  if (user === null) {
+    throw new AuthError(errorMessage);
+  }
+}
+
+module.exports = {
+  checkResourceIsFound,
+  checkUserIsAuth,
+};

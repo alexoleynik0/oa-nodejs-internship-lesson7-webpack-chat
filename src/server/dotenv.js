04e8b6edc097dd584aaab86f4f4adc6da06f7f15
Alex Oleynik <oleynik.webdev@gmail.com>
2022-08-12 05:18:39 +0300

    chore: dotenv path resolve relative;

--- src/server/dotenv.js
+++ src/server/dotenv.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
 
-const dotenvPath = path.resolve(process.cwd(), '.env');
+const dotenvPath = path.resolve(__dirname, '../..', '.env');
 
-if (fs.existsSync(dotenvPath, { encoding: 'utf8' }) !== true) {
-  const dotenvExamplePath = path.resolve(process.cwd(), '.env.example');
+if (fs.existsSync(dotenvPath) !== true) {
+  const dotenvExamplePath = path.resolve(__dirname, '../..', '.env.example');
   fs.copyFileSync(dotenvExamplePath, dotenvPath);
 }
 
